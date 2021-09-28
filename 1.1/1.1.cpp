7316d5431f09ef8f23610be94882baceeece2bd9
rezabungel <svyatchenko-arte@mail.ru>
2021-09-28 23:40:04 +0300

    Initial

---
+++ 1.1/1.1.cpp
@@ -0,0 +1,130 @@
+﻿// 1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Святченко Артём Андреевич
+
+#include <list>
+#include <iostream>
+using namespace std;
+
+bool conditions(int value) //Проверка на кратность 3. Если да, то кратное. Если нет, то не кратное.
+{
+    return (value % 3 == 0);
+}
+
+
+template<class T>
+void push(list<T>& lst, T element)//Вставка элемента с сохранением отсортированности.
+{
+    typename list<T>::iterator p = lst.begin();
+    while (p != lst.end() && *p <= element)//В итераторе нельзя одновременно идти и добавлять(удалять) значения.
+    {
+        p++;
+    }
+    //Проверяем какое из условий нарушилось.
+    if (p == lst.end())
+    {
+        lst.push_back(element);
+    }
+    else
+    {
+        lst.insert(p, element);
+    }
+}
+
+
+template<class T>
+void pop(list<T>& lst, T nomer)
+{
+    int counter = 1;
+    typename list<T>::iterator p = lst.begin();
+    while (p != lst.end())
+    {
+        if (counter == nomer)
+        {
+            break;
+        }
+        p++;
+        counter++;
+    }
+    lst.erase(p);
+}
+
+
+template<class T>
+list<T> filter(list<T> base, bool (*ptr)(T))//Фильтруем список. Условие числа кратные 3.
+{
+    list<T> result;
+    typename list<T>::iterator p = base.begin();
+    while (p != base.end())
+    {
+        if (ptr(*p))
+        {
+            result.push_back(*p);
+        }
+        p++;
+    }
+    return (result);
+}
+
+
+template<class T>
+void print(list<T> lst)//Вывод списка.
+{
+    typename list<T>::iterator p = lst.begin();
+    while (p != lst.end())
+    {
+
+        //Перемещение по контейнеру с помощью указателя.
+        cout << *p << " ";
+        p++;
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    list<int> one;
+    for (int i = 0; i < 10; i = i + 1)
+    {
+        one.push_back(i);
+    }
+    cout << "Start list: ";
+    print(one);
+
+    cout << endl;
+
+    int a = 0;
+    cout << "Test function push." << endl;
+    cout << "What number do you want to insert. a=";
+    cin >> a;
+    cout << "ok." << endl;
+    push(one, a);
+    print(one); //Вывод списка.
+
+    cout << endl;
+
+    int b = 0;
+    cout << "List size : " << one.size() << endl;
+    cout << "Kakoy po schetu element vy khotite udalit'.(chisla ot 1 do " << one.size() << ") b=";
+    cin >> b;
+    while (b<1 || b>one.size())
+    {
+        cout << "Byl vveden ne pravil'nyy nomer elementa." << endl;
+        cout << "Poprobuyte yeshcho raz." << endl;
+        cout << "Mozhno vvodit' tol'ko сhisla ot 1 do " << one.size() << ". b = ";
+        cin >> b;
+    }
+    cout << "Ok." << endl;
+    pop(one, b);
+    print(one); //Вывод списка.
+
+    cout << endl;
+
+    list<int> filtered;
+    filtered = filter(one, conditions);
+    cout << "Filtration conditions: chisla kratnyye 3." << endl;
+    cout << "Filtered list: ";
+    print(filtered); //Вывод списка.
+
+    return 0;
+}
---
+++ 1.2/1.2.cpp
@@ -0,0 +1,28 @@
+﻿// 1.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Святченко Артём Андреевич
+
+#include <list>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class school
+{
+private:
+    string surname;//фамилия
+    string name;//имя
+    string floor;//пол
+    int classroom;//класс
+    string data;//дата рождения
+    string address;//адрес(Город)
+public:
+
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    //cout << "Введи дату (дд.мм.гггг):\t"; cin >> data;
+}
