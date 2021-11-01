e8a19951fd93eb7170953ada2d621ce09d38def2
rezabungel <svyatchenko-arte@mail.ru>
2021-11-01 18:11:24 +0300

    Сompletion 2.2. Initial 2.3. Update gitignore.

--- 2.2/2.2.cpp
+++ 2.2/2.2.cpp
@@ -17,16 +17,89 @@
 
 
 #include <iostream>
+#include <string>
 #include <queue>
 
 using namespace std;
 
-template<typename T>
-void print_queue(T& q)
+class schoolboy
 {
+private:
+	string surname; //Фамилия ученика.
+	int data; //Дата рождения (год)
+public:
+	schoolboy()
+	{
+		surname = "Фамилия?";
+		data = 0;
+	}
+	schoolboy(string Xsurname, int Xdata) //Конструктор.
+	{
+		surname = Xsurname;
+		data = Xdata;
+	}
+	//Сеттеры.
+	void setsurname(string a) //Запись фамилии.
+	{
+		surname = a;
+	}
+	void setdata(int a) //Запись дату рождения (год).
+	{
+		data = a;
+	}
+	//Геттеры.
+	string getsurname() //Выдать фамилию.
+	{
+		return surname;
+	}
+	int getdata() //Выдать дату рождения (год).
+	{
+		return data;
+	}
+
+	friend bool operator < (const schoolboy& a, const schoolboy& b)
+	{
+		if (a.surname < b.surname)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	friend bool operator > (const schoolboy& a, const schoolboy& b)
+	{
+		if (a.surname > b.surname)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	friend ostream& operator<< (ostream& ustream, schoolboy& obj); //Вывод информации.
+};
+
+ostream& operator<< (ostream& ustream, schoolboy& obj)
+{
+	{
+		ustream << "Школьник:\t" << obj.getsurname() << " Дата рождения (год):\t" << obj.getdata();
+		return ustream;
+	}
+}
+
+template<typename T>
+void print_queue(T& q) //После вывода наш контейнер останеться пустым.
+{
+	schoolboy a;
 	while (!q.empty())
 	{
-		cout << q.top() << " ";
+		a = q.top();
+		cout << a << endl;
 		q.pop();
 	}
 	cout << '\n';
@@ -36,8 +109,32 @@ void print_queue(T& q)
 
 int main()
 {
-	priority_queue<int> q;
-	for (int n : {1, 8, 5, 6, 3, 4, 0, 9, 7, 2})
-		q.push(n);
+	setlocale(LC_ALL, "Russian");
+	schoolboy A("Petrov", 2002);
+	schoolboy B("Ivanov", 2005);
+	schoolboy C("Sidorov", 2007);
+	schoolboy D("Nikolaev", 2014);
+	schoolboy E("Abramov", 2012);
+	schoolboy F("Fedorov", 2001);
+	schoolboy G("Kuznetsov", 1999);
+
+	cout << "***************************************************************" << endl;
+	schoolboy test;
+	cout << "Test getsurname: " << test.getsurname() << endl;
+	cout << "Test getdata: " << test.getdata() << endl;
+	test.setsurname("ABC");
+	test.setdata(3000);
+	cout << "Test setsurname, setdata and output to the console: " << test << endl;
+	cout << "***************************************************************" << endl << endl;
+
+	priority_queue<schoolboy> q;
+	q.push(A);
+	q.push(B);
+	q.push(C);
+	q.push(D);
+	q.push(E);
+	q.push(F);
+	q.push(G);
+
 	print_queue(q);
 }
---
+++ 2.3/2.3.cpp
@@ -0,0 +1,215 @@
+﻿// 2.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*
+Постройте шаблон сбалансированного дерева. Используйте его для
+хранения объектов класса C по ключам K в соответствии с таблицей (2.3).
+Переопределите функцию вывода содержимого дерева с помощью
+итераторов (в порядке возрастания / убывания ключей). Добавьте функции
+поиска элемента по ключу, значению.
+
+Вариант 18.
+Таблица 2.3. Ключ и тип объекта, хранимого в контейнере АВЛ-дерево
+
+Ключ: Фамилия и имя
+
+Класс C: «Школьник».Минимальный набор полей: фамилия, имя, пол, класс, дата рождения, адрес.
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+//узел
+template<class T>
+class Node
+{
+protected:
+	//закрытые переменные Node N; N.data = 10 вызовет ошибку
+	T data;
+	//не можем хранить Node, но имеем право хранить указатель
+	Node* left;
+	Node* right;
+	Node* parent;
+	//переменная, необходимая для поддержания баланса дерева
+	int height;
+public:
+	//доступные извне переменные и функции
+	virtual void setData(T d) { data = d; }
+	virtual T getData() { return data; }
+	int getHeight() { return height; }
+	virtual Node* getLeft() { return left; }
+	virtual Node* getRight() { return right; }
+	virtual Node* getParent() { return parent; }
+	virtual void setLeft(Node* N) { left = N; }
+	virtual void setRight(Node* N) { right = N; }
+	virtual void setParent(Node* N) { parent = N; }
+	//Конструктор. Устанавливаем стартовые значения для указателей
+	Node<T>(T n)
+	{
+		data = n;
+		left = right = parent = NULL;
+		height = 1;
+	}
+	Node<T>()
+	{
+		left = NULL;
+		right = NULL;
+		parent = NULL;
+		data = 0;
+		height = 1;
+	}
+	virtual void print()
+	{
+		cout << "\n" << data;
+	}
+	virtual void setHeight(int h)
+	{
+		height = h;
+	}
+	template<class T> friend ostream& operator<< (ostream& stream, Node<T>& N);
+};
+template<class T>
+ostream& operator<< (ostream& stream, Node<T>& N)
+{
+	stream << "\nNode data: " << N.data << ", height: " << N.height;
+	return stream;
+}
+template<class T>
+void print(Node<T>* N) { cout << "\n" << N->getData(); }
+template<class T>
+class Tree
+{
+protected:
+	//корень - его достаточно для хранения всего дерева
+	Node<T>* root;
+public:
+	//доступ к корневому элементу
+	virtual Node<T>* getRoot() { return root; }
+	//конструктор дерева: в момент создания дерева ни одного узла нет, корень смотрит в никуда
+	Tree<T>() { root = NULL; }
+	//рекуррентная функция добавления узла. Устроена аналогично, но вызывает сама себя - добавление в левое или правое поддерево
+	virtual Node<T>* Add_R(Node<T>* N)
+	{
+		return Add_R(N, root);
+	}
+	virtual Node<T>* Add_R(Node<T>* N, Node<T>* Current)
+	{
+		if (N == NULL) return NULL;
+		if (root == NULL)
+		{
+			root = N;
+			return N;
+		}
+		if (Current->getData() > N->getData())
+		{
+			//идем влево
+			if (Current->getLeft() != NULL)
+				Current->setLeft(Add_R(N, Current->getLeft()));
+			else
+				Current->setLeft(N);
+			Current->getLeft()->setParent(Current);
+		}
+		if (Current->getData() < N->getData())
+		{
+			//идем вправо
+			if (Current->getRight() != NULL)
+				Current->setRight(Add_R(N, Current->getRight()));
+			else
+				Current->setRight(N);
+			Current->getRight()->setParent(Current);
+		}
+		if (Current->getData() == N->getData())
+			//нашли совпадение
+			;
+		//для несбалансированного дерева поиска
+		return Current;
+	}
+	//функция для добавления числа. Делаем новый узел с этими данными и вызываем нужную функцию добавления в дерево
+	virtual void Add(int n)
+	{
+		Node<T>* N = new Node<T>;
+		N->setData(n);
+		Add_R(N);
+	}
+	virtual Node<T>* Min(Node<T>* Current = NULL)
+	{
+		//минимум - это самый "левый" узел. Идём по дереву всегда влево
+		if (root == NULL) return NULL;
+		if (Current == NULL)
+			Current = root;
+		while (Current->getLeft() != NULL)
+			Current = Current->getLeft();
+		return Current;
+	}
+	virtual Node<T>* Max(Node<T>* Current = NULL)
+	{
+		//минимум - это самый "правый" узел. Идём по дереву всегда вправо
+		if (root == NULL) return NULL;
+		if (Current == NULL)
+			Current = root;
+		while (Current->getRight() != NULL)
+			Current = Current->getRight();
+		return Current;
+	}
+	//поиск узла в дереве. Второй параметр - в каком поддереве искать, первый - что искать
+	virtual Node<T>* Find(int data, Node<T>* Current)
+	{
+		//база рекурсии
+		if (Current == NULL) return NULL;
+		if (Current->getData() == data) return Current;
+		//рекурсивный вызов
+		if (Current->getData() > data) return Find(data, Current->getLeft());
+		if (Current->getData() < data) return Find(data, Current->getRight());
+	}
+	//три обхода дерева
+	virtual void PreOrder(Node<T>* N, void (*f)(Node<T>*))
+	{
+		if (N != NULL)
+			f(N);
+		if (N != NULL && N->getLeft() != NULL)
+			PreOrder(N->getLeft(), f);
+		if (N != NULL && N->getRight() != NULL)
+			PreOrder(N->getRight(), f);
+	}
+	//InOrder-обход даст отсортированную последовательность
+	virtual void InOrder(Node<T>* N, void (*f)(Node<T>*))
+	{
+		if (N != NULL && N->getLeft() != NULL)
+			InOrder(N->getLeft(), f);
+		if (N != NULL)
+			f(N);
+		if (N != NULL && N->getRight() != NULL)
+			InOrder(N->getRight(), f);
+	}
+	virtual void PostOrder(Node<T>* N, void (*f)(Node<T>*))
+	{
+
+		if (N != NULL && N->getLeft() != NULL)
+			PostOrder(N->getLeft(), f);
+		if (N != NULL && N->getRight() != NULL)
+			PostOrder(N->getRight(), f);
+		if (N != NULL)
+			f(N);
+	}
+};
+
+int main()
+{
+	Tree<double> T;
+	int arr[15];
+	int i = 0;
+	for (i = 0; i < 15; i++) arr[i] = (int)(100 * cos(15 * double(i + 1)));
+	for (i = 0; i < 15; i++)
+		T.Add(arr[i]);
+	Node<double>* M = T.Min();
+	cout << "\nMin = " << M->getData() << "\tFind " << arr[3] << ": " <<
+		T.Find(arr[3], T.getRoot());
+	void (*f_ptr)(Node<double>*); f_ptr = print;
+	cout << "\n-----\nInorder:";
+	T.InOrder(T.getRoot(), f_ptr);
+	char c; cin >> c;
+	return 0;
+}
